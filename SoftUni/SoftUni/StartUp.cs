8f68c36ae0449f09437d909ded6359f91dd75007
Yusmen <mojito@abv.bg>
2020-11-06 21:27:38 +0200

    Adding Database First solutions

---
+++ SoftUni/SoftUni/StartUp.cs
@@ -0,0 +1,46 @@
+﻿
+using SoftUni.Data;
+using System;
+using System.Linq;
+using System.Text;
+
+namespace SoftUni
+{
+    public class StartUp
+    {
+        public static void Main(string[] args)
+        {
+            using (var softUniContext=new SoftUniContext())
+            {
+                Console.WriteLine(GetEmployeesFullInformation(softUniContext));
+            }
+            
+            
+        }
+
+        public static string GetEmployeesFullInformation(SoftUniContext context)
+        {
+            var employees = context.Employees
+                .Select(x=>new
+                {
+                    x.FirstName,
+                    x.MiddleName,
+                    x.LastName,
+                    x.JobTitle,
+                    x.Salary,
+                    x.EmployeeId
+                })
+                .OrderBy(x => x.EmployeeId);
+            StringBuilder stringBuilder = new StringBuilder();
+
+            foreach(var employee in employees)
+            {
+                stringBuilder.AppendLine($"{employee.FirstName} {employee.MiddleName} {employee.LastName} {employee.JobTitle} {employee.Salary:f2}");
+            }
+
+            return stringBuilder.ToString().TrimEnd();
+
+          
+        }
+    }
+}
