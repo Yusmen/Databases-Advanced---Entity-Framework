343a10d8ce0b6b5c6dafbba1170424b157b72d5a
Yusmen <mojito@abv.bg>
2021-02-23 12:44:31 +0200

    Adding Football Bettig project only with moldels for migrations

---
+++ EntittyRelations/P03_FootballBetting.Data.Models/PlayerStatistic.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace P03_FootballBetting.Data.Models
+{
+    public class PlayerStatistic
+    {
+       
+        public int GameId { get; set; }
+
+        public Game Game { get; set; }
+
+        public int PlayerId { get; set; }
+
+        public Player Player { get; set; }
+        public byte ScoredGoalsId { get; set; }
+        public byte Assists { get; set; }
+        public byte MinutesPlayed { get; set; }
+
+        
+    }
+}
